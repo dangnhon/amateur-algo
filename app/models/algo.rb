02283c4bc021cac10dbfcf9bf83191c873410ad7
dangnhon <dang.nhon0308@gmail.com>
2022-09-15 22:30:29 -0700

    set up structure for type sort on index

--- app/models/algo.rb
+++ app/models/algo.rb
@@ -1,2 +1,14 @@
 class Algo < ApplicationRecord
+
+# There should only be a ifxed number of type
+# will have to alte the form to preload the types avail
+# so users can choose. Below function will sort based on type
+# which will be used to sort on the index page in each of the 
+# cards. Then they should be able to expand the cards to 
+# view all problems associated with said category of algorithms.
+
+    def soted_by_type_of
+
+    end
+
 end
